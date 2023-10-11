e3c7e0ce0fc47a74b246663ebe717c82c55115f8
DMITRII POPOV <bulpopov@gmail.com>
2023-10-11 20:33:56 +0300

    lisää linkityksen merkinnän muokkaussivulle

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,5 +1,6 @@
 import styles from './Item.module.scss'
 import { MdNavigateNext } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 function Item({ data, ...props }) {
     const locale = "fi-FI"
@@ -28,7 +29,7 @@ function Item({ data, ...props }) {
                 <div className={styles.item_average}>{average ? average + "/kk" : ""}</div>
             </div>
             <div className={styles.item_edit}>
-                <MdNavigateNext />
+                <Link to={"/edit/" + data.id}><MdNavigateNext /></Link>
             </div>
         </div>
     )
